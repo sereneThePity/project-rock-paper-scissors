012698ef632a73bc1a4ffb1840fc81b760555ee4
Serene The Pity <weekndsonn@gmail.com>
2023-07-09 12:35:53 +0200

    Add a function game: plays for 3 rounds, and declares the winner

--- index.js
+++ index.js
@@ -8,33 +8,67 @@ getComputerChoice = () => {
 singleRound = (playerSelection,computerSelection) => {
     playerSelection = prompt("choose your weapon","").toLowerCase();
     computerSelection = getComputerChoice();
+    let win = 0, loss = 0, draw = 0
     console.log(computerSelection)
 
     if (playerSelection === 'rock' && computerSelection === 'paper'){
         console.log('Get Wrapped');
+        loss++;
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        console.log('You ROCK!')
+        console.log('You ROCK!');
+        win++;
     }
     else if (playerSelection === 'rock' && computerSelection === 'rock'){
-        console.log('Smash Bros: fight again')
+        console.log('Smash Bros: fight again');
+        draw++;
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        console.log('Get Cut')
+        console.log('Get Cut');
+        loss++;
     }
     else if (playerSelection === 'paper' && computerSelection === 'paper'){
-        console.log('Paper Mache')
+        console.log('Paper Mache');
+        draw++;
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        console.log('You wRap?!!')
+        console.log('You wRap?!!');
+        win++;
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-        console.log('Get Stoned')
+        console.log('Get Stoned');
+        loss++;
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        console.log('Cutting Time')
+        console.log('Cutting Time');
+        win++;
     }
-    else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
-        console.log('Scissoring')
+    else {
+        console.log('Scissoring');
+        draw++;
     }
+
+    score = [win,loss,draw]
+
+    return score
+}
+
+game = ()=> {
+    
+    let w,l,d;
+    for(i=1;i<4;i++){
+        singleRound()
+        w=score[0],l=score[1],d=score[2];
+        w+=score[0];
+        l+=score[1];
+        d+=score[2];
+    }
+
+    if (w>l && w>d){
+        console.log('You win');
+    }
+    else if (l>w && l>d){
+        console.log('Computer wins');
+    }
+    else console.log('Draw');
 }
